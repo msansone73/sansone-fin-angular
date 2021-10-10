9e65dee783e0b3119e3b424cde8a0774897ec57c
msansone <msansone@gmail.com>
2021-10-10 12:55:47 +0100

    added menu/routing draft

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './component/home/home.component';
+import { ListStockComponent } from './component/stock/list-stock/list-stock.component';
+import { ListTransactionComponent } from './component/transaction/list-transaction/list-transaction.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'list-stock', component: ListStockComponent},
+  {path: 'list-transaction', component: ListTransactionComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
